2a869c1f2f975012b9962743eb3f0493a205c948
Emmanuel Moran <emmanuel.moran@uabc.edu.mx>
2018-11-29 08:30:53 -0800

    updates on firebase auth

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import { Text, Button, View, TextInput } from 'react-native';
+import { Text, Button, View, TextInput, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import firebase from '@firebase/app';
 import '@firebase/auth';
-import { Spinner, Input } from '../common/Spinner';
+import { Spinner } from '../common/Spinner';
 
+const DismissKeyboard = ({ children }) => (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      {children}
+    </TouchableWithoutFeedback>
+  );
+  
 class LoginForm extends Component {
     state = { email: '', password: '', error: '', loading: false };
 
@@ -14,15 +20,11 @@ class LoginForm extends Component {
 
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(this.onLoginSuccess.bind(this))
-        .catch(() => {
-            firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(this.onLoginSuccess.bind(this))
-            .catch(this.onLoginFail.bind(this));
-        });
+        .catch(this.onLoginFail.bind(this));
     }
 
     onLoginFail() {
-        this.setState({ error: 'Authentication failed', loading: false });
+        this.setState({ error: 'Credenciales incorrectas', loading: false });
     }
 
     onLoginSuccess() {
@@ -35,42 +37,45 @@ class LoginForm extends Component {
     }
 
     renderButton() {
-        if (this.state.loading) {
-            return <Spinner size="small" />;
-        }
+        // if (this.state.loading) {
+        //     return <Spinner size="small" />;
+        // }
         
         return (
-            <Button title='Log in' onPress={this.onButtonPress.bind(this)} />
+            <Button style={styles.buttonText} title='Log in' onPress={this.onButtonPress.bind(this)} />
         );
     }
 
     render() {
+        const { container, containerInputs, errorTextStyle, input, buttonContainer } = styles;
         return (
-            <View>
-                <View>
+            <DismissKeyboard>
+            <View style={container}>
+                <View style={containerInputs}>
                     <TextInput
+                        style={input}
                         placeholder="user@email.com"
-                        label="Email" 
                         value={this.state.email}
                         onChangeText={email => this.setState({ email })}
                     />
                     <TextInput
                         secureTextEntry
-                        label="Password"
+                        style={input}
                         placeholder="********"
                         value={this.state.password}
                         onChangeText={password => this.setState({ password })}
                     />
                 </View>
 
-                <Text style={styles.errorTextStyle}>
+                <Text style={errorTextStyle}>
                     {this.state.error}
                 </Text>
 
-                <View>
+                <View style={buttonContainer}>
                     {this.renderButton()}
                 </View>
             </View>
+            </DismissKeyboard>
         );
     }
 }
@@ -80,7 +85,33 @@ const styles = {
         fontSize: 20,
         alignSelf: 'center',
         color: 'red'
-    }
+    },
+    container: {
+        justifyContent: 'center',
+        paddingTop: '30%',
+    },
+    containerInputs: {
+        backgroundColor: '#fff',
+      },
+      input: {
+        color: '#000',
+        fontSize: 16,
+        marginHorizontal: 35,
+        marginVertical: 10,
+        borderBottomColor: '#000',
+        borderBottomWidth: 0.2,
+      },
+      buttonContainer: {
+        marginHorizontal: 35,
+      },
+      buttonText: {
+        color: '#fff',
+        fontWeight: '900',
+        fontSize: 14,
+        padding: 8,
+        alignText: 'center',
+        backgroundColor: '#607D8B',
+      },
 };
 
 export default LoginForm;
