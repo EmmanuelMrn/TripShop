40d6b4af4ea19549b47b92a785b99771a4ff64a3
Emmanuel Moran <emmanuel.moran@uabc.edu.mx>
2018-11-29 02:36:47 -0800

    login

--- src/screens/AdminScreen.js
+++ src/screens/AdminScreen.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Button } from 'react-native';
+//import * as firebase from 'firebase';
+import firebase from '@firebase/app';
+import LoginForm from '../components/login';
 import HeaderComponent from '../components/header';
+import Spinner from '../components/common/Spinner';
 
-export default class AdminContainer extends Component {
-   render() {
-    return (
-        <View>
+class AdminContainer extends Component {
+    state = {
+        loggedIn: null
+    };
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCcLmlNKTCehAYCqUtId5yPmh3w2MVSQaM',
+            authDomain: 'authreactnative-75113.firebaseapp.com',
+            databaseURL: 'https://authreactnative-75113.firebaseio.com',
+            projectId: 'authreactnative-75113',
+            storageBucket: 'authreactnative-75113.appspot.com',
+            messagingSenderId: '93985325380'
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+          case true:
+            return (
+                <View>
+                    <Button title="Log out" onPress={() => firebase.auth().signOut()} />
+                </View>
+            );
+          case false:
+            return <LoginForm />;
+          default:
+            return <Spinner size="large" />;
+        }
+      }
+    
+    render() {
+        return (
+          <View>
             <HeaderComponent navigation={this.props.navigation} title={'Administrador'} />
-            <Text> Admin Screen</Text>
-        </View>
-        
-    );
-  }
+            {this.renderContent()}
+          </View>
+        );
+    }
 }
+
+export default AdminContainer;
